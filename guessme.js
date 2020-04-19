c9678e083be3e0ecd906d0e753cc0cdfb3308306
Harisaipravin <harisaipravin@gmail.com>
2020-04-19 21:10:30 +0530

    GuessMyPeer:Comment words

--- guessme.js
+++ guessme.js
@@ -6,8 +6,18 @@ function Node(data, y, n) {
   
   var readlineSync = require('readline-sync');
   var fs = require("fs");
+
+
+  var words = ["That's great!", "You know my peers!", "Let's play again!", "One more try!"];
+  var word = words[Math.floor(Math.random() * words.length)];  
+
+  var thanks = ["Thanks!", "Nice one!"];
+  var thank = thanks[Math.floor(Math.random() * words.length)];
+
+  var playAgain = ["Let's play again!", "That was fun, let's play again!"]
+  var playAgainLoad = playAgain[Math.floor(Math.random() * words.length)];
   
-  var tree = fs.readFileSync('tree.json');
+  var tree = fs.readFileSync('data.json');
   var root = JSON.parse(tree);
   var node;
   
